de9feab2bdd4c11712960c36097974f0080dbcf6
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2018-05-08 15:48:13 -0400

    added video detail, apil works, can select videos to show

---
+++ src/components/video_detail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
+  const videoId = video.id.videoId; // will give linting error - read it and decide for yourself
+  // {videoId} = video.id // is example of destructuring
+  const url = `https://www.youtube.com/embed/${videoId}`;
+
+
+  return (
+    <div id="video-detail">
+      <iframe src={url} title="video-detail" />
+      <div className="details">
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
--- src/index.js
+++ src/index.js
@@ -5,17 +5,32 @@ import './style.scss';
 // const $ = require('jquery');
 
 import SearchBar from './components/search_bar';
-
+import youtubeSearch from './youtube-api';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}; // nothing here yet
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    youtubeSearch('pixar').then((videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
   }
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />
       </div>
     );
   }
